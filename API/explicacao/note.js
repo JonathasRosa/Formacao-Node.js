297ce6f9fca9d5acb561219780a4644254b9a211
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-04-25 11:10:59 -0300

    Update Formação NODE.js

---
+++ API/explicacao/note.js
@@ -0,0 +1,22 @@
+/*
+> HTTP
+    - É um protocolo de transferencia de dados na web que permite a comunicação do usuário com o servidor.
+    funcionado com requisição e resposta.
+>> VERBOS HTTP, são as forma de como vc faz as sua requisições na web.
+    São tipos de requisição diferente, GET, POST, PUT, PATCH,  DELETE.
+>>> STATUS CODE
+    É a forma de dizer para quem fez a requisição para a API o que acontceu. 
+>>>> WEBSERVICE
+    É uma API que funciona na web através do protocolo HTTP.
+>>>>> API 
+    é uma interface de comunicação entre duas ou mais aplicações.
+>>>>>> REST
+    * É um padrão de desenvolvimento de webservis, com 5 regras *
+    - Deve seguir a >arquitetura cliente/servidor< (ele não pode ser as duas coisas ao mesmo tempo)
+    - >Stateless< (Não se deve garadar estado do cliente, pega a requisição respone e esquece o cliente)
+    - Ela tem que permitir fazer cache de informações na API, (cache, economiza dados de processamento no servidor voltado para desempenho)
+    - Saber trabalhar com camadas
+    - Interfaces uniformes e diretas.
+    >>>>>>> RESTFUL
+        É um padrão de desenvolvimento de webservis que vai além das 5 regras já mencionadas. 
+*/
