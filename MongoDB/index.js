e240c7b22d938249a88932ddcf52e746248ce6b3
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-05-10 11:11:35 -0300

    Update Formação Node.JS

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,3 +1,25 @@
 const mongoose = require ("mongoose");
+const articleModel = require("./article")
 
 mongoose.connect("mongodb://localhost:27017/", {useNewUrlParser: true}, {useUnifiedTopology: true});
+
+const Article = mongoose.model("Article", articleModel);
+
+
+// Cadastro
+const artigo = new Article({
+    title: "Olá mundo", 
+    author:"Jhon", 
+    body:"jbdcsb",
+    especial: true,
+    resume: {
+        content:"bla bla bla",
+        author: "Teste!!"
+    }
+});
+
+artigo.save().then(()=>{
+    console.log("Artigo salvo com sucesso!");
+}).catch(err =>{
+    console.log(err);
+});
